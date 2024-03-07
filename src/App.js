d5fda0d083e4d25d97e0aa1e2bfdc0598e4929dd
Roman Gudz <alien31roman@gmail.com>
2024-03-07 22:17:23 +0300

    add eslint and  propTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import Header from "./components/Header";
-import Main from "./components/Main";
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
