adc8a46d00b71b9831a0582602d9f63bdab4436a
Roman Gudz <alien31roman@gmail.com>
2024-04-04 15:44:44 +0300

    edit URL

--- src/API/const.js
+++ src/API/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'dvewARA5TwCZ9b9s3Yja9w';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-ruby.vercel.app/auth';
 export const SCOPE = 'identity submit read';
