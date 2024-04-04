215172f828f5a4acd65b69b6f5c9417eb246cf86
Roman Gudz <alien31roman@gmail.com>
2024-04-04 21:18:58 +0300

    delete preloader

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,6 @@
 import style from './Home.module.css';
-import DotLoader from 'react-spinners/RingLoader';
 
 export const Home = () => (<div>
   <h1 className={style.container}>Стартовая страница</h1>
   <h3 className={style.h3}>Добро пожаловать на Blogget</h3>
-  <DotLoader color='orange' />
 </div>);
